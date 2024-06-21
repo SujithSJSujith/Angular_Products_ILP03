feae9a2489b83acf3e80a609bc7380c7afad0ab1
SujithSj <sujithloginout@gmail.com>
2024-06-21 09:30:02 +0530

    updated add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductComponent } from './ui/product/product.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'dashboard2';
+  
+    ngOnInit():void{
+      this.fetchProduct()
+    }
+  
+    productList:any = [];
+    fetchProduct = async () => {
+      try{
+          let response = await fetch("https://dummyjson.com/products");
+          let pList = await response.json()
+          this.productList = pList.products;
+          console.log(this.productList)
+        
+      }
+      catch(err){
+          console.log("Error is : ",err)
+      }
+    }
+  
+  
 }
